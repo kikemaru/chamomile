69d1b4ade1d7951141dcc1c212c60f8dda1922be
Roman <duirole@mail.ru>
2023-06-24 23:29:58 +0300

    👾 bash file for running commands to configure the configuration

--- setting.sh
+++ setting.sh
@@ -9,5 +9,5 @@ ip_address=$1
 proxy_address=$2
 file_path="./_docker/nginx/conf.d/nginx.conf"
 
-awk -v ip="$ip_address" 'NR==17 {$0="allow " ip ";"} 1' "$file_path" > temp && mv temp "$file_path"
-awk -v proxy="$proxy_address" 'NR==19 {$0="proxy_pass " proxy ";"} 1' "$file_path" > temp && mv temp "$file_path"
+awk -v ip="$ip_address" 'NR==17 {$0="        allow " ip ";"} 1' "$file_path" > temp && mv temp "$file_path"
+awk -v proxy="$proxy_address" 'NR==19 {$0="        proxy_pass " proxy ";"} 1' "$file_path" > temp && mv temp "$file_path"
